d1ed1d625cae98b516c8a55a4f46f7f2d30c3a79
Ruhong <seahrh@gmail.com>
2021-09-05 17:34:55 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0"
+setup(
+    name="question-answering",
+    version=__version__,
+    python_requires="~=3.7",
+    install_requires=[
+        "optuna==2.9.1",
+        "pandas==1.3.2",
+        "pyarrow==5.0.0",
+        "scikit-learn==0.24.2",
+        "pytorch-lightning==1.4.5",
+        "transformers==4.10.0",
+        "tqdm==4.62.2",
+    ],
+    extras_require={
+        "tests": [
+            "black==21.8b0",
+            "mypy==0.910",
+            "pytest==6.2.5",
+            "pytest-cov==2.12.1",
+        ],
+        "notebook": ["jupyterlab==1.2.16", "ipywidgets==7.6.3", "seaborn==0.11.1"],
+    },
+    packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    description="Question answering",
+    license="MIT",
+    author="seahrh",
+    author_email="seahrh@gmail.com",
+    url="https://github.com/seahrh/question-answering",
+)
